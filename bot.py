d7303408f4d49122a402da55e15d8154d040aaa5
Zane Othman-Gomez <zane.othman@gmail.com>
2021-08-01 22:39:07 -0700

    rearrange bot.py & add config.example.toml

--- bot.py
+++ bot.py
@@ -9,21 +9,21 @@ import toml
 
 class PG13Bot(commands.Bot):
     def __init__(self, config_path):
-        config = toml.load(config_path)
+        # Get bot root logger
+        self.logger = logging.getLogger("pg13")
 
-        # Choose only necessary intents
+        # Load TOML configuration
+        config = toml.load(config_path)
+        self.guild_configs = config["guilds"]
+        self._token = config["token"]
+
+        # Initialize discord.py side of bot
         bot_intents = discord.Intents(
             guild_messages=True, voice_states=True, guilds=True, members=True
         )
+        super().__init__(command_prefix=config["prefix"], intents=bot_intents)
 
-        super().__init__(
-            command_prefix=config["commands"]["prefix"], intents=bot_intents
-        )
-        self.logger = logging.getLogger("pg13")
-        self.guild_ids = config["commands"]["guilds"]
-        self._token = config["token"]
-
-        # Initialize slash commands
+        # Set up slash commands
         self.slash = SlashCommand(self, sync_commands=True)
 
         # Ensure database folder exists
