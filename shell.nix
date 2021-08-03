1400dde23128ca7c63338368d407837caf160ae4
Zane Othman-Gomez <zane.othman@gmail.com>
2021-08-03 13:49:59 -0700

    Add discord-py-slash-command to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,17 +1,23 @@
-{ pkgs ? import <unstable> { } }:
+{ pkgs ? import <nixpkgs> { } }:
 
-pkgs.mkShell {
+let
+  discord-py-slash-command = pkgs.python39Packages.buildPythonPackage rec {
+    pname = "discord-py-slash-command";
+    version = "2.4.0";
+
+    src = pkgs.python39Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "133g2bdi7h3kl3d6whs64pc5zs7g29kq8rw9ycpq0x2yjrgbp4sm";
+    };
+
+    buildInputs = with pkgs.python39Packages; [ aiohttp discordpy ];
+  };
+  customPython = pkgs.python39.withPackages
+    (ps: [ discord-py-slash-command ps.discordpy ps.toml ps.aiosqlite ]);
+in pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
-    python39Full
-    python39Packages.pip
-    python39Packages.discordpy
-    python39Packages.toml
-    python39Packages.aiosqlite
-
-    # For formatting
-    python39Packages.black
-
-    # Just for statistics
-    scc
+    customPython
+    black # Formatting
+    scc # Just for statistics
   ];
 }
