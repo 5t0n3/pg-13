705b8c31913e4ba19674241b3e5b81b3b4edd992
Zane Othman-Gomez <zane.othman@gmail.com>
2021-12-21 20:01:00 -0800

    Rotate log files every day at midnight
    This is done to avoid overwriting old files whenever the bot restarts,
    as well as to divide the files based on the day rather than having one
    monolithic log file.

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import logging
+import logging.handlers as handlers
 
 from bot import PG13Bot
 
-# Set up logging
-logging.basicConfig(
+# Rotate log files every 24 hours (at midnight)
+rotating_handler = handlers.TimedRotatingFileHandler(
     filename="discord.log",
-    encoding="utf-8",
-    filemode="w",
-    format="[{levelname}] ({name}:{lineno}) {asctime}: {message}",
-    style="{",
-    level="INFO",
+    when="midnight",
 )
+
+# Use INFO logging level
+rotating_handler.setLevel("INFO")
+
+# Set up custom logging format
+log_format = logging.Formatter(
+    fmt="[{levelname}] ({name}:{lineno}) {asctime}: {message}",
+    style="{",
+)
+rotating_handler.setFormatter(log_format)
+
+# Add handler to root logger
+root_logger = logging.getLogger()
+root_logger.addHandler(rotating_handler)
 
 pg13_bot = PG13Bot("config.toml")
 pg13_bot.run()
