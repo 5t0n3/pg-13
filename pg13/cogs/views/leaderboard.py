ec63b87fe738d693466622702c2e8db75f82bc70
Zane Othman-Gomez <zane.othman@gmail.com>
2022-08-03 17:20:06 -0700

    Possibly remove users that left a server from the leaderboard?
    I tried to make it so that there are still 15 people per page, but
    that logic is really complicated and probably doesn't work as I
    intended.

--- pg13/cogs/views/leaderboard.py
+++ pg13/cogs/views/leaderboard.py
@@ -1,14 +1,34 @@
+import collections
 import functools
 
 import discord
+
+ScoreInfo = collections.namedtuple("ScoreInfo", ["member", "score"])
 
 
 def build_embed(base_str, member_info):
     place = member_info[0]
     score_info = member_info[1]
-    username = "[user left]" if score_info[0] is None else score_info[0].display_name
 
-    return base_str + f"{place}: {username} - {score_info[1]}\n"
+    return (
+        base_str + f"{place}: {score_info.member.display_name} - {score_info.score}\n"
+    )
+
+
+def filter_members(bundles, users_needed):
+    valid_members = []
+    total_users = 0
+
+    for bundle in bundles:
+        if bundle.member is not None:
+            valid_members.append(bundle.member)
+
+        total_users += 1
+
+        if len(valid_members) == users_needed:
+            break
+
+    return valid_members, skips
 
 
 class Leaderboard(discord.ui.View):
@@ -16,7 +36,18 @@ class Leaderboard(discord.ui.View):
         super().__init__()
         self.guild = guild
         self.db_pool = db_pool
-        self.offset = 0
+        self.page = 0
+
+        # TODO: should scores be stored directly instead?
+        self.offsets = [0]
+
+    @property
+    def current_offset(self):
+        return self.offsets[self.page]
+
+    @property
+    def current_offset(self):
+        return self.offsets[self.page + 1]
 
     async def init_leaderboard(self, interaction):
         async with self.db_pool.acquire() as con:
@@ -27,10 +58,16 @@ class Leaderboard(discord.ui.View):
             )
 
         bundled_users = [
-            (self.guild.get_member(row["userid"]), row["score"]) for row in user_scores
+            ScoreInfo(self.guild.get_member(row["userid"]), row["score"])
+            for row in user_scores
         ]
-        self.current_users = bundled_users[:15]
-        self.next_users = bundled_users[15:]
+        valid_users, next_offset = filter_members(bundled_users, 15)
+        self.current_users = valid_users
+        self.offsets.append(next_offset)
+
+        valid_next_users, lookahead = filter_members(bundled_users[next_offset:])
+        self.next_users = valid_next_users
+        self.lookahead_length = lookahead
 
         self.leaderboard_right.disabled = len(self.next_users) == 0
 
@@ -43,11 +80,13 @@ class Leaderboard(discord.ui.View):
         await interaction.response.send_message(embed=leaderboard_embed, view=self)
 
     async def update(self, interaction):
-        self.leaderboard_left.disabled = self.offset == 0
+        self.leaderboard_left.disabled = self.offsets[self.page] == 0
         self.leaderboard_right.disabled = len(self.next_users) == 0
 
         leaderboard = functools.reduce(
-            build_embed, enumerate(self.current_users, start=self.offset + 1), ""
+            build_embed,
+            enumerate(self.current_users, start=self.current_offset + 1),
+            "",
         )
         leaderboard_embed = discord.Embed(
             title=f"{self.guild.name} Leaderboard", description=leaderboard
@@ -59,19 +98,21 @@ class Leaderboard(discord.ui.View):
         self, interaction: discord.Interaction, button: discord.ui.Button
     ):
         self.next_users = self.current_users
-        self.offset -= 15
+        self.page -= 1
 
         async with self.db_pool.acquire() as con:
             unbundled_current = await con.fetch(
                 "SELECT userid, score FROM scores WHERE guild = $1 "
-                "ORDER BY score DESC, userid DESC OFFSET $2 ROWS FETCH NEXT 15 ROWS ONLY",
+                "ORDER BY score DESC, userid DESC OFFSET $2 ROWS FETCH NEXT $3 ROWS ONLY",
                 self.guild.id,
-                self.offset,
+                self.current_offset,
+                self.next_offset - self.current_offset,
             )
 
         self.current_users = [
-            (self.guild.get_member(row["userid"]), row["score"])
+            ScoreInfo(member, row["score"])
             for row in unbundled_current
+            if (member := self.guild.get_member(row["userid"])) is not None
         ]
 
         await self.update(interaction)
@@ -81,19 +122,70 @@ class Leaderboard(discord.ui.View):
         self, interaction: discord.Interaction, button: discord.ui.Button
     ):
         self.current_users = self.next_users
-        self.offset += 15
+        self.page += 1
 
-        async with self.db_pool.acquire() as con:
-            unbundled_next = await con.fetch(
-                "SELECT userid, score FROM scores WHERE guild = $1 "
-                "ORDER BY score DESC, userid DESC OFFSET $2 ROWS FETCH NEXT 15 ROWS ONLY",
-                self.guild.id,
-                self.offset + 15,
+        if len(self.current_users) < 15:
+            async with self.db_pool.acquire() as con:
+                unbundled_complement = await con.fetch(
+                    "SELECT userid, score FROM scores WHERE guild = $1 "
+                    "ORDER BY score DESC, userid DESC OFFSET $2 ROWS FETCH NEXT $3 ROWS ONLY",
+                    self.guild.id,
+                    self.current_offset + len(self.next_users),
+                    15 - len(self.next_users) + 15,
+                )
+
+            raw_unbundled = [
+                ScoreInfo(self.guild.get_member(row["userid"]), row["score"])
+                for row in unbundled_complement
+            ]
+
+            current_complement, total_complement = filter_members(
+                raw_unbundled, 15 - len(self.current_users)
             )
+            self.current_users.extend(current_complement)
 
-        self.next_users = [
-            (self.guild.get_member(row["userid"]), row["score"])
-            for row in unbundled_next
-        ]
+            self.offsets.append(
+                self.current_offset + self.lookahead_length + total_complement
+            )
+            raw_next_bundles = raw_unbundled[total_complement:]
+
+        # An offset exists 2 pages ahead of this one
+        elif len(self.offsets) > self.page + 2:
+            async with self.db_pool.acquire() as con:
+                unbundled_next = await con.fetch(
+                    "SELECT userid, score FROM scores WHERE guild = $1 "
+                    "ORDER BY score DESC, userid DESC OFFSET $2 ROWS FETCH NEXT $3 ROWS ONLY",
+                    self.guild.id,
+                    self.next_offset,
+                    self.offsets[self.page + 2] - self.next_offset,
+                )
+
+            self.next_users = [
+                ScoreInfo(self.guild.get_member(row["userid"]), row["score"])
+                for row in unbundled_next
+                if (member := self.guild.get_member(row["userid"])) is not None
+            ]
+
+            return await self.update(interaction)
+
+        else:
+            async with self.db_pool.acquire() as con:
+                unbundled_next = await con.fetch(
+                    "SELECT userid, score FROM scores WHERE guild = $1 "
+                    "ORDER BY score DESC, userid DESC OFFSET $2 ROWS FETCH NEXT 15 ROWS ONLY",
+                    self.guild.id,
+                    self.next_offset,
+                )
+
+            raw_next_bundles = [
+                ScoreInfo(self.guild.get_member(row["userid"]), row["score"])
+                for row in unbundled_next
+            ]
+
+            self.offsets.append(self.current_offset + self.lookahead_length)
+
+        valid_next, lookahead = filter_members(raw_next_bundles, 15)
+        self.next_users = valid_next
+        self.lookahead_length = lookahead
 
         await self.update(interaction)
