2f1c63e882800e7230815a2fe53862263d3715e2
Zane Othman <zane.othman@gmail.com>
2022-12-04 19:11:43 -0800

    Bump poetry dependencies/flake inputs & switch nix formatter
    For some reason discord.py and systemd-python broke due to setuptools
    missing, so I was able to add it in via poetry2nix's override system.
    Not quite sure what changed last deploy though.

--- flake.nix
+++ flake.nix
@@ -8,76 +8,102 @@
     };
   };
 
-  outputs = { self, nixpkgs, utils, flake-compat }:
-    utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "i686-linux" ]
-    (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        packages.pg-13 = pkgs.poetry2nix.mkPoetryApplication {
-          projectDir = ./.;
-          python = pkgs.python310;
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    flake-compat,
+  }:
+    utils.lib.eachSystem ["x86_64-linux" "aarch64-linux" "i686-linux"]
+    (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.pg-13 = pkgs.poetry2nix.mkPoetryApplication {
+        projectDir = ./.;
+        python = pkgs.python310Full;
 
-        devShells.default = pkgs.mkShell {
-          packages = [ self.packages.${system}.pg-13 pkgs.black ];
-        };
+        # TODO: figure out why this broke
+        overrides = [
+          pkgs.poetry2nix.defaultPoetryOverrides
+          (final: prev: {
+            systemd-python = prev.systemd-python.overridePythonAttrs (old: {
+              buildInputs = old.buildInputs ++ [pkgs.python310Packages.setuptools];
+            });
+            discord-py = prev.discord-py.overridePythonAttrs (old: {
+              buildInputs = old.buildInputs ++ [pkgs.python310Packages.setuptools];
+            });
+          })
+        ];
+      };
 
-        formatter = pkgs.nixfmt;
-      }) // {
-        nixosModules.default = { config, pkgs, lib, ... }:
-          with lib;
-          let cfg = config.services.pg-13;
-          in {
-            options.services.pg-13 = {
-              enable = mkOption {
-                type = types.bool;
-                default = false;
-                description = "Whether to start the PG-13 Discord bot on boot.";
-              };
+      devShells.default = pkgs.mkShell {
+        packages = [self.packages.${system}.pg-13 pkgs.black];
+      };
 
-              configFile = mkOption {
-                type = types.path;
-                default = "/var/lib/pg-13/config.toml";
-                description = "The path to the PG-13 bot configuration.";
-              };
+      formatter = pkgs.alejandra;
+    })
+    // {
+      nixosModules.default = {
+        config,
+        pkgs,
+        lib,
+        ...
+      }:
+        with lib; let
+          cfg = config.services.pg-13;
+        in {
+          options.services.pg-13 = {
+            enable = mkOption {
+              type = types.bool;
+              default = false;
+              description = "Whether to start the PG-13 Discord bot on boot.";
             };
 
-            config = mkIf cfg.enable {
-              users.groups.pg-13 = { };
-              users.users.pg-13 = {
-                isSystemUser = true;
-                group = "pg-13";
-                extraGroups = [ "postgres" ];
-                home = "/var/lib/pg-13";
-                createHome = true;
-              };
-
-              systemd.services.pg-13 = {
-                enable = true;
-                description = "the PG-13 point system bot";
-                wants = [ "network-online.target" ];
-                after = [ "network-online.target" "postgresql.service" ];
-                wantedBy = [ "multi-user.target" ];
-
-                serviceConfig = {
-                  User = "pg-13";
-                  WorkingDirectory = "/var/lib/pg-13";
-                  ExecStart = "${self.packages.${pkgs.system}.pg-13}/bin/pg-13";
-                  Environment = "CONFIG_PATH=${cfg.configFile}";
-                };
-              };
-
-              services.postgresql = {
-                enable = true;
-                package = pkgs.postgresql_14;
-
-                ensureDatabases = [ "pg_13" ];
-                ensureUsers = [{
-                  name = "pg-13";
-                  ensurePermissions = { "DATABASE pg_13" = "ALL PRIVILEGES"; };
-                }];
-              };
+            configFile = mkOption {
+              type = types.path;
+              default = "/var/lib/pg-13/config.toml";
+              description = "The path to the PG-13 bot configuration.";
             };
           };
-      };
+
+          config = mkIf cfg.enable {
+            users.groups.pg-13 = {};
+            users.users.pg-13 = {
+              isSystemUser = true;
+              group = "pg-13";
+              extraGroups = ["postgres"];
+              home = "/var/lib/pg-13";
+              createHome = true;
+            };
+
+            systemd.services.pg-13 = {
+              enable = true;
+              description = "the PG-13 point system bot";
+              wants = ["network-online.target"];
+              after = ["network-online.target" "postgresql.service"];
+              wantedBy = ["multi-user.target"];
+
+              serviceConfig = {
+                User = "pg-13";
+                WorkingDirectory = "/var/lib/pg-13";
+                ExecStart = "${self.packages.${pkgs.system}.pg-13}/bin/pg-13";
+                Environment = "CONFIG_PATH=${cfg.configFile}";
+              };
+            };
+
+            services.postgresql = {
+              enable = true;
+              package = pkgs.postgresql_14;
+
+              ensureDatabases = ["pg_13"];
+              ensureUsers = [
+                {
+                  name = "pg-13";
+                  ensurePermissions = {"DATABASE pg_13" = "ALL PRIVILEGES";};
+                }
+              ];
+            };
+          };
+        };
+    };
 }
