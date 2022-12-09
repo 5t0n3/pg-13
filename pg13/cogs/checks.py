1341c3a98e80c57d8a2050c17242143ede4668c7
Zane Othman <zane.othman@gmail.com>
2022-12-09 12:39:40 -0800

    Make bot owner an admin for slash command purposes

--- pg13/cogs/checks.py
+++ pg13/cogs/checks.py
@@ -6,7 +6,8 @@ from ..config import admins
 async def admin_check(interaction: discord.Interaction):
     """Returns true if the user attempting to use a command is a bot admin"""
     guild_admins = admins[interaction.guild_id]
+    is_bot_owner = await interaction.client.is_owner(interaction.user)
 
-    return interaction.user.id in guild_admins["users"] or not set(
+    return is_bot_owner or interaction.user.id in guild_admins["users"] or not set(
         map(lambda role: role.id, interaction.user.roles)
     ).isdisjoint(set(guild_admins["roles"]))
